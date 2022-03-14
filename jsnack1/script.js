7775394544026a379df2684e303ed910f3e5aba4
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-14 15:39:08 +0100

    add first exercise complete

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,3 +4,23 @@ console.log("JS OK!");
 // consegna:
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo all'utente di inserire una parola
+const inputParola = prompt("inserisci parola");
+
+// valorizzo palindroma con il valore di ritorno di wordPalindrome
+const palindroma = wordPalindrome(inputParola);
+
+// se il valore restituito dalla funzione corrisponde alla parola inserita,
+// la parola risulterà palindroma,altrimenti No
+
+if (inputParola == palindroma) {
+  console.log(`la parola ${palindroma} è palindroma`);
+} else {
+  console.log(`la parola ${palindroma} non è palindroma`);
+}
+// richiamo la funzione
+function wordPalindrome(word) {
+  let wordInverse = word.split("").reverse().join("");
+  return wordInverse;
+}
