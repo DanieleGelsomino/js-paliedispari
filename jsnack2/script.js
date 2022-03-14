d907b665fa9432e9eb68993b634a517d277fde50
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-14 17:04:02 +0100

    add second exercise complete

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -7,3 +7,47 @@ console.log("JS OK!");
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+// chiedo all'utente di scegliere pari o dispari
+const inputScelta = prompt("scegli Pari o Dispari?");
+console.log(`Hai scelto ${inputScelta}`);
+
+// chideo un numero da 1 a 5
+const inputNumero = prompt("inserisci un numero da 1 a 5");
+console.log(`Hai scelto il numero ${inputNumero}`);
+
+// genero numero random da 1 a 5 per il computer usando la funzione
+const numeroCom = generaNumeroRandom(1, 5);
+console.log(`il Computer ha scelto il numero ${numeroCom}`);
+
+// sommo i numeri dell'utente e del computer
+const sommaNumUserCom = inputNumero + numeroCom;
+
+console.log(`la somma del numero User e del numero Com è ${sommaNumUserCom}`);
+// verifico che il risultato sia pari o dispari
+const pariODispari = evenOrOdd(inputNumero, numeroCom);
+
+// se il valore del risultato della funzione sarà uguale all'inputScelta l'utente vince,
+// altrimenti vince il computer
+if (pariODispari === inputScelta) {
+  console.log("Hai Vinto!");
+} else {
+  console.log("Hai Perso!");
+}
+
+// richiamo la funzione per generare numeri random
+function generaNumeroRandom(min, max) {
+  const range = max - min + 1;
+  const numGenerated = Math.floor(Math.random() * range) + min;
+  return numGenerated;
+}
+
+// richiamo la funzione per verificare se la somma dei numeri è pari o dispari
+function evenOrOdd(numUser, numCom) {
+  let somma = numUser + numCom;
+  if (somma % 2 === 0) {
+    return "pari";
+  } else {
+    return "dispari";
+  }
+}
